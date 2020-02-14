c70026bfbac1814a71d6b5d522a70bb6086be754
Tomas <tomas.muser@gmail.com>
2020-02-14 03:39:45 -0300

    Cambio en  el algoritmo para simplificacion

--- codeSignal/arrayOne.py
+++ codeSignal/arrayOne.py
@@ -3,43 +3,20 @@ import random
 
 def arrayGenerator():
     a = []
-#    lenght = random.randint(1, pow(10,5))
-#    print(lenght)
-    for i in range(20):
-        a.append(random.randint(1,5))
+    lenght = random.randint(1, pow(10,5))
+    for i in range(lenght):
+        a.append(random.randint(1, lenght))
     return a
 
 
 def firstDuplicate(a):
     dicc = {}
-    limite = len(a)
     for i in range(len(a)):
-        if i < limite:
-            for j in range(len(a)):
-                if i < j:
-                    if a[i] not in dicc:
-                        if a[i] == a[j]:
-                            limite = j
-                            dicc[a[i]] = j
-                    else:
-                        break
-                else:
-                    break
+        if a[i] in dicc:
+            return a[i]
         else:
-            break
-    duplicados = list(dicc.keys())
-    if len(duplicados) > 0:
-        menor = dicc[duplicados[0]]
-        for i in range(len(duplicados)):
-            if i > 0:
-                if menor > dicc[duplicados[i]]:
-                    menor = dicc[duplicados[i]]
-        return a[menor]
-    else:
-        return -1
-
-
+            dicc[a[i]] = i
+    return -1
 
 a = arrayGenerator()
-print(a)
 print(firstDuplicate(a))
